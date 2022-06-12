a7d7095134ccf567da21beb292324d72be098761
Zia Uddin <90231496+UddinZia@users.noreply.github.com>
2022-06-12 18:33:24 -0500

    Update matrix.h

--- matrices/matrix.h
+++ matrices/matrix.h
@@ -8,7 +8,6 @@ using std::vector;
 
 class Matrix {
 	private:
-		// Student's implementation
 		vector<vector<int>> matrix; 
 		size_t rows;
 		size_t columns;
@@ -20,23 +19,19 @@ class Matrix {
 		void file_to_matrix(char* filename);
 		void matrix_to_file(char* filename);
 
-		// Task 1
 		std::vector<int> multiply_vector(std::vector<int> B);
 		int count_mults(std::vector<int> B);
 		int count_adds(std::vector<int> B);
 
-		// Task 2
 		Matrix multiply(Matrix B);
 		Matrix transpose();
 		int count_mults(Matrix B);
 		int count_adds(Matrix B);
 		bool is_multipliable(Matrix B);
 
-		// Task 3
 		Matrix kronecker(Matrix B);
 		int count_mults_kron(Matrix B);
 
-		// my own functions
 		size_t getRows() const;
 		size_t getColumns() const;
 		void displayMatrix () const;
